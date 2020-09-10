70c5f23592ff19f8f5935e76ac903932d6d5d104
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-10 17:02:09 +0200

    Preparing navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,67 @@
 import React from 'react';
+import logo from '../images/logo.png'
+// import {
+//     makeStyles
+// } from '@material-ui/core'
+
+// const useStyles = makeStyles (() => ({
+//     logo : {
+//         height : 80,
+//         width : 80
+//     }
+// }))
 
 export const Navbar = () => {
+    // const classes = useStyles ()
     return (
-        <div className = 'root' style = {{}}>
-            
-        </div>
+        <nav id='menu' className = 'navbar navbar-default navbar-fixed-top'>
+            <div className = 'container'>
+                <div className = 'navbar-header'>
+                    <button
+                        type = 'button'
+                        className = 'navba-toggle collapsed'
+                        data-toggle = 'collapse'
+                        data-target = '#bs-example-navbar-collapse-1'
+                    >
+                        {" "}
+                        <span className="sr-only">Toggle navigation</span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                        <span className="icon-bar"></span>{" "}
+                    </button>
+                    <a href = '#menu' className = 'navbar-brand page-scroll'>
+                        <img src = {logo} alt = '' style = {{ height : 90, width : 90, backgroundColor : '#ffffff'}} />
+                    </a>
+                </div>
+                <div
+                    className="collapse navbar-collapse"
+                    id="bs-example-navbar-collapse-1"
+                >
+                    <ul className = 'nav navbar-nav navbar-right'>
+                        <li>
+                            <a href = '#top' className = 'page-scroll'>
+                                Accueil
+                            </a>
+                        </li>
+                        <li>
+                            <a href = '#top' className = 'page-scroll'>
+                                A propos
+                            </a>
+                        </li>
+                        <li>
+                            <a href = '#top' className = 'page-scroll'>
+                                Vision
+                            </a>
+                        </li>
+                        <li>
+                            <a href = '#top' className = 'page-scroll'> 
+                                Contacts
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
