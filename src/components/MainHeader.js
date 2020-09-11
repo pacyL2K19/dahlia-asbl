bc53950625928796fac13327cc1262133b8b9b8b
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-11 17:31:52 +0200

    Configuring the helper file

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Donate} from './Donate';
 import {
-    Container,
     Grid
 } from '@material-ui/core'
 
---
+++ src/components/SupportTransition.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const SuportTransition = () => {
+    return (
+        <div id = 'supportTtansition'>
+            <div className = 'transitionElement'>
+
+            </div>
+            <div className = 'transitionElement'>
+                
+            </div>
+            <div className = 'transitionElement'>
+
+            </div>
+        </div>
+    )
+}
