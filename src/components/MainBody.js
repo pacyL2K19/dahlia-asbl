7f626e4c32267d7a30d77cf37b9e29b6809cfa5a
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-12 03:01:51 +0200

    Preparing first section of the body

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,13 +1,74 @@
 import React from 'react';
+import {
+    Grid
+} from '@material-ui/core'
 
 const MainBody = () => {
     return (
         <div id = 'mainBody'>
             <div id = 'firstContent'>
-                 
+                <Grid container>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}
+                    >
+                        <h3 style = {{textAlign : 'end'}}>
+                            Nous travaillons <br />
+                            Auprès de tous ceux qui sont<br />
+                            Dans le besoin <br />
+                        </h3>
+                    </Grid>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}
+                    >
+                        <p style = {{textAlign : 'start', color : '#5a5a5a'}}>
+                            Nous travaillons auprès de tout ceux qui sont dans le besoin : Contribuer à une société́ congolaise dynamique où la femme sera libre, forte, valorisée et apportera du progrès , donner l’accès à l’éducation aux enfants pour leur assurer un avenir meilleur et un regard innovant autour des valeurs du travail.
+                        </p>
+                    </Grid>
+                </Grid>
             </div>
             <div id = 'iconContents'>
-                 
+                 <Grid
+                    container
+                 >
+                    <Grid
+                        item
+                        className = 'itemIconContent'
+                        xs = {12}
+                        md = {12}
+                        lg = {4}
+                        xl = {4}
+                    >
+
+                    </Grid>
+                    <Grid
+                        item
+                        className = 'itemIconContent'
+                        xs = {12}
+                        md = {12}
+                        lg = {4}
+                        xl = {4}
+                    >
+
+                    </Grid>
+                    <Grid
+                        item
+                        className = 'itemIconContent'
+                        xs = {12}
+                        md = {12}
+                        lg = {4}
+                        xl = {4}
+                    >
+
+                    </Grid>
+                 </Grid>
             </div>
             <div id = 'videoBan'>
                  
