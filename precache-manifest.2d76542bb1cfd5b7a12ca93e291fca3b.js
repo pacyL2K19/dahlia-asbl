9cb8a5861c5aa3ff9fa81960fae07bfd31aa0bdb
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-13 14:22:18 +0200

    Updates

---
+++ precache-manifest.2d76542bb1cfd5b7a12ca93e291fca3b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e65f297a7d7282269fc2f818afdb14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8e06bed3fb0ecad96c3",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "590461a1c72f5edeb012",
+    "url": "/static/js/2.8a690b42.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8a690b42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e06bed3fb0ecad96c3",
+    "url": "/static/js/main.04ac42c8.chunk.js"
+  },
+  {
+    "revision": "841d122faf410e946dce",
+    "url": "/static/js/runtime-main.6ff20899.js"
+  },
+  {
+    "revision": "04f09d473d19b706f702c811d0c2aa23",
+    "url": "/static/media/bgHome.04f09d47.jpg"
+  },
+  {
+    "revision": "edde8839488359bdcb805e32700e82c4",
+    "url": "/static/media/bgHome1.edde8839.jpg"
+  },
+  {
+    "revision": "1741164704c00811524987b84e1f7a49",
+    "url": "/static/media/first.17411647.jpg"
+  },
+  {
+    "revision": "e320655d15378b815a99b89384d4e193",
+    "url": "/static/media/logo.e320655d.png"
+  },
+  {
+    "revision": "8055307d38774aeb5ff7b730f3f077c7",
+    "url": "/static/media/playing.8055307d.gif"
+  },
+  {
+    "revision": "cc6da07f2bd051094cab7e5b9a68f18d",
+    "url": "/static/media/second.cc6da07f.jpg"
+  },
+  {
+    "revision": "703be360c610b7d6d83f5bbf8eb7006d",
+    "url": "/static/media/third.703be360.jpg"
+  },
+  {
+    "revision": "0edc2496e906f03a63e0c0a3bb8a0a62",
+    "url": "/static/media/videoScreen.0edc2496.png"
+  }
+]);
