b674d7e83b02b068bc551862d1478e9c15a3e31d
Pacifique Linjanja <pacilinja1@icloud.com>
2020-10-29 00:13:24 +0200

    Design Project page

---
+++ src/views/Eng/Projects.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import PreFooter from '../../components/Eng/PreFooter';
+import Footer from '../../components/Eng/Footer'; 
+
+//images 
+
+import vision from '../../images/pro.png';
+import support from '../../images/Volunteer.png';
+import donataire from '../../images/donataire.png';
+import dialogue from '../../images/dialogue.png';
+import cerveau from '../../images/cerveau.png';
+import { Grid } from '@material-ui/core';
+
+const ProjectsE = () => {
+    return (
+        <div>
+            <div id = 'introAboutE' style = {{backgroundImage : 'url('+require('../../images/bgVision.jpg')+')', backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', display : 'flex', justifyContent : 'center', alignItems : 'center', flexDirection : 'column'}}>
+                <img src = {vision} alt = ''/><br /><br />
+                <h2>Our Projects</h2>
+            </div>
+            <div id = 'mainContent' className = 'container'>
+                <Grid container>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}  
+                        style = {{display : 'flex', justifyContent : 'center', flexDirection : 'column', paddingRight : 30}}  
+                    >
+                        <p style = {{textAlign: "center", color: '# 733b83', fontSize: 28, fontWeight: 'bold'}}> Our projects in planning and execution in Kinshasa </p> <br /> < br />
+                        <p style = {{textAlign: "justify"}}> • The construction of a school (center) for vocational training for poor young girls and single mothers; </p> <br />
+                        <p style = {{textAlign: "justify"}}> • Empowerment of Congolese women through entrepreneurship; </p> <br />
+                        <p style = {{textAlign: "justify"}}> • Farming in rural areas to fight against malnutrition; </p> <br />
+                        <p style = {{textAlign: "justify"}}> • The implementation of the Oréal Paris company in Kinshasa. </p> <br />
+                    </Grid>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}    
+                    >
+                        <div style = {{backgroundImage : 'url('+require('../../images/pro.svg')+')', backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', height : 400}}>
+
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+            <PreFooter />
+            <Footer />
+        </div>
+    )
+}
+
+export default ProjectsE
---
+++ src/views/Projects.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import PreFooter from '../components/PreFooter';
+import Footer from '../components/Footer'; 
+
+//images 
+
+import vision from '../images/pro.png';
+import support from '../images/Volunteer.png';
+import donataire from '../images/donataire.png';
+import dialogue from '../images/dialogue.png';
+import cerveau from '../images/cerveau.png';
+import { Grid } from '@material-ui/core';
+
+const Projects = () => {
+    return (
+        <div>
+            {/* <Navbar /> */}
+            <div id = 'introAbout' style = {{backgroundImage : 'url('+require('../images/bgVision.jpg')+')', backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', display : 'flex', justifyContent : 'center', alignItems : 'center', flexDirection : 'column'}}>
+                <img src = {vision} alt = ''/><br /><br />
+                <h2>Nos Projets</h2>
+            </div>
+            <div id = 'mainContent' className = 'container'>
+                <Grid container>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}  
+                        style = {{display : 'flex', justifyContent : 'center', flexDirection : 'column', paddingRight : 30}}  
+                    >
+                        <p style = {{textAlign:"center", color : '#733b83', fontSize : 28, fontWeight:'bold'}}>Nos projets en cours de planification et exécution à Kinshasa</p><br /><br />
+                        <p style = {{textAlign:"justify"}}>• La construction d’une école (d’un centre ) de formations professionnelles pour jeunes filles, filles-mères démunies ;</p><br />
+                        <p style = {{textAlign:"justify"}}>• L’autonomisation de la femme congolaise par l’entrepreneuriat;</p><br />
+                        <p style = {{textAlign:"justify"}}>• L’exploitation agricole en milieu rural pour lutter contre la malnutrition;</p><br />
+                        <p style = {{textAlign:"justify"}}>• L’implémentation de l’entreprise Oréal Paris à Kinshasa.</p><br />
+                    </Grid>
+                    <Grid 
+                        item
+                        xs = {12}
+                        md = {12}
+                        lg = {6}
+                        xl = {6}    
+                    >
+                        <div style = {{backgroundImage : 'url('+require('../images/pro.svg')+')', backgroundPosition : 'center', backgroundSize : '98%', backgroundRepeat : 'no-repeat', height : 400}}>
+
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+            <PreFooter />
+            <Footer />
+        </div>
+    )
+}
+
+export default Projects
