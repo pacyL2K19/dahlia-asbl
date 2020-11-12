5379ca286f6a066340fcd031e6cc451b27d41c21
Pacifique Linjanja <pacilinja1@icloud.com>
2020-11-12 15:11:30 +0200

    Update fr version project page

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -6,6 +6,11 @@ import Footer from '../components/Footer';
 
 import vision from '../images/pro.png';
 import gifPlayer from '../images/playing.gif';
+import { Grid } from '@material-ui/core';
+import agri from '../images/agri.jpg';
+import auto from '../images/autonomie.jpg';
+import ecole from '../images/ecole.jpg';
+import loreal from '../images/loreal.jpg';
 
 const Projects = () => {
     return (
@@ -16,7 +21,7 @@ const Projects = () => {
                 <h2>Nos Projets</h2>
             </div>
             <div style={{height : 450, backgroundImage : 'url('+require('../images/realisations.jpg')+')', backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', paddingTop : 300, borderRadius : 5}}>
-                <div className = 'videoBack' style = {{ backgroundImage : 'url('+require('../images/Famille.jpg')+')',  backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', width : '40%', marginLeft : 'auto', marginRight : 'auto', height : 400, marginBottom : 20, display : 'flex', alignItems : 'center', justifyContent : 'center'}}>
+                <div className = 'videoBack' style = {{ backgroundImage : 'url('+require('../images/boss1.jpg')+')',  backgroundPosition : 'center', backgroundSize : 'cover', backgroundRepeat : 'no-repeat', width : '35%', marginLeft : 'auto', marginRight : 'auto', height : 500, marginBottom : 20, display : 'flex', alignItems : 'center', justifyContent : 'center'}}>
                     <a rel = 'noopener noreferrer' target = '_blank' href = 'https://www.youtube.com/watch?v=73Yqk3ytXMs&t=32s'>
                         <img
                             style={{height:80, width : 80, borderRadius : 40}}
@@ -27,12 +32,54 @@ const Projects = () => {
                     </a>
                 </div>
             </div>
-            <div id = 'mainContent' className = 'container' style={{width : '40%', marginLeft : 'auto', marginRight : 'auto', marginTop : 300}}>
+            <div id = 'mainContent' style={{marginTop : 380}}>
                 <p style = {{textAlign:"center", color : '#733b83', fontSize : 28, fontWeight:'bold'}}>Nos projets en cours de planification et exécution à Kinshasa</p><br /><br />
-                <p style = {{textAlign:"justify"}}>• La construction d’une école (d’un centre ) de formations professionnelles pour jeunes filles, filles-mères démunies ;</p><br />
-                <p style = {{textAlign:"justify"}}>• L’autonomisation de la femme congolaise par l’entrepreneuriat;</p><br />
-                <p style = {{textAlign:"justify"}}>• L’exploitation agricole en milieu rural pour lutter contre la malnutrition;</p><br />
-                <p style = {{textAlign:"justify"}}>• L’implémentation de l’entreprise <b>l'Oréal Paris</b> à Kinshasa.</p><br />
+                <Grid container style={{pading: 50}}>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {ecole} alt = 'image' style={{width: '100%'}} />
+                        <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>La construction d’une école (d’un centre ) de formations professionnelles pour jeunes filles, filles-mères démunies</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {auto} alt = 'image' style={{width: '100%'}} />
+                        <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’autonomisation de la femme congolaise par l’entrepreneuriat</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {agri} alt = 'image' style={{width: '100%'}} />
+                        <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’exploitation agricole en milieu rural pour lutter contre la malnutrition</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {loreal} alt = 'image' style={{width: '100%'}} />
+                        <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’implémentation de l’entreprise <b>l'Oréal Paris</b> à Kinshasa.</p>
+                    </Grid>
+                </Grid>
             </div>
             <PreFooter />
             <Footer />
