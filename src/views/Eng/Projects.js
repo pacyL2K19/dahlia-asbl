9bcb6acb5a4ccc0c349ebd062774226458bf809e
Pacifique Linjanja <pacilinja1@icloud.com>
2020-11-28 13:34:39 +0200

    Update the project page eng version

--- src/views/Eng/Projects.js
+++ src/views/Eng/Projects.js
@@ -34,7 +34,98 @@ const ProjectsE = () => {
             </div>
             <div id = 'mainContent' className = 'container' style={{marginTop : 380}}>
                 <p style = {{textAlign: "center", color: '#733b83', fontSize: 35, fontWeight: 'bold'}}> Our projects in planning and execution in Kinshasa </p> <br />
-                <Grid container style={{pading: 50}}>
+                <Grid container style={{pading: 80}} className="container">
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        {/* <img src = {ecole} alt = 'image' style={{width: '100%', height: 400}} /> */}
+                        <p style = {{textAlign:"justify", fontSize: 28, fontWeight: 'bold', marginTop: 15}}>The construction of a school (center) for vocational training for poor young girls and single mothers</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {ecole} alt = 'image' style={{width: '100%', height: 400}} />
+                        {/* <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’autonomisation de la femme congolaise par l’entrepreneuriat</p> */}
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <p style = {{textAlign:"justify", fontSize: 28, fontWeight: 'bold', marginTop: 15}}>Empowerment of Congolese women through entrepreneurship</p>
+                        {/* <img src = {agri} alt = 'image' style={{width: '100%', height: 400}} /> */}
+                        {/* <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’exploitation agricole en milieu rural pour lutter contre la malnutrition</p> */}
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {auto} alt = 'image' style={{width: '100%', height: 400}} />
+                        {/* <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’implémentation de l’entreprise <b>l'Oréal Paris</b> à Kinshasa.</p> */}
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        {/* <img src = {ecole} alt = 'image' style={{width: '100%', height: 400}} /> */}
+                        <p style = {{textAlign:"justify", fontSize: 28, fontWeight: 'bold', marginTop: 15}}>Farming in rural areas to fight against malnutrition</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {agri} alt = 'image' style={{width: '100%', height: 400}} />
+                        {/* <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’autonomisation de la femme congolaise par l’entrepreneuriat</p> */}
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        {/* <img src = {ecole} alt = 'image' style={{width: '100%', height: 400}} /> */}
+                        <p style = {{textAlign:"justify", fontSize: 28, fontWeight: 'bold', marginTop: 15}}>The implementation of the <b>l'Oréal Paris company</b> in Kinshasa.</p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs = {12}
+                        md = {6}
+                        lg = {6}
+                        xl = {6}
+                        style={{padding: 10}}
+                    >
+                        <img src = {loreal} alt = 'image' style={{width: '100%', height: 400}} />
+                        {/* <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>L’autonomisation de la femme congolaise par l’entrepreneuriat</p> */}
+                    </Grid>
+                </Grid>
+                {/* <Grid container style={{pading: 50}}>
                     <Grid
                         item
                         xs = {12}
@@ -79,7 +170,7 @@ const ProjectsE = () => {
                         <img src = {loreal} alt = 'image' style={{width: '100%', height: 400}} />
                         <p style = {{textAlign:"justify", fontWeight: 'bold', marginTop: 15}}>The implementation of the <b>l'Oréal Paris company</b> in Kinshasa.</p>
                     </Grid>
-                </Grid>
+                </Grid> */}
             </div>
             <PreFooter />
             <Footer />
